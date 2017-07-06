a4c9a6a8856a1514d1195027ce8af888095c51f0
Constance Gobert <constancegobert@hotmail.fr>
2017-07-06 14:57:49 +0200

    tests on wiki controller action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,17 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_action :authenticate_user!
+  #before_action :authenticate_user!
 
+  include SessionsHelper
+  
+  private
 
+  def require_sign_in
+    unless current_user
+      flash[:alert] = "You must be logged in to do that"
+      redirect_to new_session_path
+    end
+  end
 
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -12,13 +12,6 @@ module SessionsHelper
      User.find_by(id: session[:user_id]) # current_user is a shortcut to this functionality
    end
 
-   private
-
-   def require_sign_in #redirect un-signed-in users. Defined here because we'll call it from other controllers.
-     unless current_user
-       flash[:alert] = "You must be logged in to do that"
-       redirect_to new_session_path
-     end
-   end
+   
 
 end
